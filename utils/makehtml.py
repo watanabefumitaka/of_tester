a7353a36f627554706b1fb195b554e20bb6df4b4
Yuichi Ito <ito.yuichi0@gmail.com>
2013-11-22 10:57:30 +0900

    HTML作成ツールを更新。
    複数のログファイルを横断的に見ることができるので、暫定的に必要。
    
    Signed-off-by: Yuichi Ito <ito.yuichi0@gmail.com>

--- utils/makehtml.py
+++ utils/makehtml.py
@@ -7,111 +7,166 @@ import re
 
 from xml.dom import minidom
 
-
-TEST_START_LOG = '--- Test start ---'
 LOG_DIR = './log'
 OUT_FILE = './result.html'
 
 DATE_RE = '[0-9]{4}-[0-9]{2}-[0-9]{2} ' \
-    + '[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} \[INFO\] '
+    + '[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} \\[INFO\\] '
+
 
 class TestResult(object):
     def __init__(self, logfile):
         stat = os.stat(logfile)
         last_modified = stat.st_mtime
-        dt = datetime.datetime.fromtimestamp(last_modified)
+        dtime = datetime.datetime.fromtimestamp(last_modified)
         self.file_name = os.path.basename(logfile)
-        self.last_modified = dt.strftime("%Y-%m-%d %H:%M:%S")
-        f = open(logfile, 'r')
-        lines = f.readlines()
-        f.close()
-        num = 0
-        for line in lines:
-            if TEST_START_LOG in line:
-                break
-            num += 1
-        lines = lines[num:]
+        self.last_modified = dtime.strftime("%Y-%m-%d %H:%M:%S")
+        ifile = open(logfile, 'r')
+        lines = ifile.readlines()
+        ifile.close()
         self.results = {}
         for line in lines:
             if '\033[' in line:
                 line = re.sub(DATE_RE, '', line)
                 line = re.sub('\\033\[[0-9]{1,2}m', '', line)
                 line = line.replace('\n', '')
-                (key, value) = line.split(' : ')
-                self.results[key] = value
+                (key, value_title) = line.split(' : ')
+                if -1 != value_title.find(' '):
+                    (value, title) = value_title.split(' ', 1)
+                else:
+                    value = value_title
+                    title = None
+                self.results[key] = {'value': value, 'title': title}
 
 
-def main():
-
+def parse_args():
     parser = argparse.ArgumentParser(
-        description='Create html file including the results of of_tester.')
+        description='Create html file including the results of '
+                    'of_tester.')
     parser.add_argument(
         '-l', '--log-dir', default=LOG_DIR, type=str,
-        help='directory where the log files exist. (default: %s)' % LOG_DIR,
+        help='directory where the log files exist. '
+             '(default: %s)' % LOG_DIR,
         dest='log_dir')
     parser.add_argument(
         '-o', '--out-file', default=OUT_FILE, type=str,
         help='output html file. (default: %s)' % OUT_FILE,
         dest='out_file')
     args = parser.parse_args()
+    return args
 
-    files = os.listdir(args.log_dir)
 
+def read_dir(path):
+    files = sorted(os.listdir(path))
     results = []
-    for file in files:
-        result = TestResult(LOG_DIR + os.sep + file)
+    for logfile in files:
+        if logfile.endswith('~'):
+            continue
+        result = TestResult(path + os.sep + logfile)
         results.append(result)
+    return results
 
+
+def make_keys(results):
     temp_keys = []
     for result in results:
         temp_keys.append(sorted(result.results))
     keys = max(temp_keys)
+    return keys
 
-    dom = minidom.getDOMImplementation()
-    doc = dom.createDocument(None, "html", None)
-    html = doc.documentElement
+
+def make_html(keys, results):
+    doc = minidom.Document()
+    html = doc.createElementNS('http://www.w3.org/1999/xhtml', 'html')
+    html.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml')
+    doc.appendChild(html)
+    head = doc.createElement('head')
+    html.appendChild(head)
+    title = doc.createElement('title')
+    head.appendChild(title)
+    title_text = doc.createTextNode('of_tester results')
+    title.appendChild(title_text)
+    style = doc.createElement('style')
+    style.setAttribute('type', 'text/css')
+    head.appendChild(style)
+    css = """
+    table, th, td { border: 1px #000000 solid; }
+    td { text-align: center; }
+    td.ok { background-color: #88ff88; }
+    td.ng { background-color: #ff8888; }
+    """
+    style_text = doc.createComment(css)
+    style.appendChild(style_text)
     body = doc.createElement('body')
     html.appendChild(body)
-    table = doc.createElement('table')
+    table = make_table(keys, results, doc)
     body.appendChild(table)
-    tr = doc.createElement('tr')
-    table.appendChild(tr)
-    th = doc.createElement('th')
-    tr.appendChild(th)
-    th_text = doc.createTextNode('file_name')
-    th.appendChild(th_text)
-    for result in results:
-        th = doc.createElement('th')
-        tr.appendChild(th)
-        th_text = doc.createTextNode(result.file_name)
-        th.appendChild(th_text)
-    tr = doc.createElement('tr')
-    table.appendChild(tr)
-    td = doc.createElement('td')
-    tr.appendChild(td)
-    td_text = doc.createTextNode('last_modified')
-    td.appendChild(td_text)
-    for result in results:
-        td = doc.createElement('td')
-        tr.appendChild(td)
-        td_text = doc.createTextNode(result.last_modified)
-        td.appendChild(td_text)
-    for key in keys:
-        tr = doc.createElement('tr')
-        table.appendChild(tr)
-        td = doc.createElement('td')
-        tr.appendChild(td)
-        td_text = doc.createTextNode(key)
-        td.appendChild(td_text)
-        for result in results:
-            td = doc.createElement('td')
-            tr.appendChild(td)
-            td_text = doc.createTextNode(result.results.get(key, '&nbsp;'))
-            td.appendChild(td_text)
+    return doc
 
-    f = open(args.out_file, 'w')
-    f.write(doc.toprettyxml())
-    f.close()
+
+def make_table(keys, results, doc):
+    table = doc.createElement('table')
+    tre = doc.createElement('tr')
+    table.appendChild(tre)
+    the = doc.createElement('th')
+    tre.appendChild(the)
+    th_text = doc.createTextNode('file_name')
+    the.appendChild(th_text)
+    for result in results:
+        the = doc.createElement('th')
+        tre.appendChild(the)
+        th_text = doc.createTextNode(result.file_name)
+        the.appendChild(th_text)
+    tre = doc.createElement('tr')
+    table.appendChild(tre)
+    tde = doc.createElement('td')
+    tre.appendChild(tde)
+    td_text = doc.createTextNode('last_modified')
+    tde.appendChild(td_text)
+    for result in results:
+        comment = doc.createComment(result.file_name)
+        tre.appendChild(comment)
+        tde = doc.createElement('td')
+        tre.appendChild(tde)
+        td_text = doc.createTextNode(result.last_modified)
+        tde.appendChild(td_text)
+    for key in keys:
+        tre = doc.createElement('tr')
+        table.appendChild(tre)
+        tde = doc.createElement('td')
+        tre.appendChild(tde)
+        td_text = doc.createTextNode(key)
+        tde.appendChild(td_text)
+        for result in results:
+            comment = doc.createComment(result.file_name)
+            tre.appendChild(comment)
+            tde = doc.createElement('td')
+            tre.appendChild(tde)
+            value_title = result.results.get(key)
+            if value_title:
+                td_text = doc.createTextNode(value_title['value'])
+                if 'OK' == value_title['value']:
+                    tde.setAttribute('class', 'ok')
+                else:
+                    tde.setAttribute('class', 'ng')
+                tde.appendChild(td_text)
+                if value_title['title'] is not None:
+                    tde.setAttribute('title', value_title['title'])
+    return table
+
+
+def write_html(outfile, doc):
+    ofile = open(outfile, 'w')
+    ofile.write(doc.toprettyxml())
+    ofile.close()
+
+
+def main():
+    args = parse_args()
+    results = read_dir(args.log_dir)
+    keys = make_keys(results)
+    doc = make_html(keys, results)
+    write_html(args.out_file, doc)
 
 
 if '__main__' == __name__:
